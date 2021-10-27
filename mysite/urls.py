a78d83e82a9d3b59b3f38709d57f91cbcab8c4c0
Ubuntu <ubuntu@ip-172-31-39-95.us-east-2.compute.internal>
2021-10-27 10:22:08 +0000

    url에서 check 제거

--- mysite/urls.py
+++ mysite/urls.py
@@ -31,5 +31,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('profiles.urls')),
     path('', include('getImage.urls')),
-    #path('', include('checkImage.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
